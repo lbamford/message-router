8ec5b2e86bc6cc1d7aa557492018cfa2335434b9
Lee B <lbamford@infinitasweb.co.uk>
2023-03-14 14:52:37 +0000

    Delete clean-up.sh

--- camel-message-router/ocp-deployment/clean-up.sh
+++
@@ -1,7 +0,0 @@
-NAMESPACE=poc
-oc project $NAMESPACE
-oc delete pod --field-selector=status.phase==Succeeded -n $NAMESPACE
-oc delete pod --field-selector=status.phase==Failed -n $NAMESPACE
-oc adm prune deployments --keep-complete=1 --keep-failed=0 -n $NAMESPACE --confirm
-oc adm prune builds --orphans --keep-complete=1 --keep-failed=0 -n $NAMESPACE --confirm
-oc delete -n $NAMESPACE $(oc get all | grep replicaset.apps | awk '{if ($2 + $3 + $4 == 0) print $1}')
